f404c3b3a4fefc1c88a1f190ebfb57e39de738cb
David Ta <david.ta555@gmail.com>
2022-03-24 17:48:37 -0400

    Error handling

--- src/com/javapractice/Main.java
+++ src/com/javapractice/Main.java
@@ -9,21 +9,42 @@ public class Main {
         final byte MONTHS_IN_YEARS = 12;
         final byte PERCENT = 100;
 
+        int principal;
+        float monthlyInterest;
+        int numberOfPayments;
+
         Scanner scanner = new Scanner(System.in);
 
         //Principal
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1000000)
+                break;
+            System.out.println("Enter an amount between $1,000 and $1,000,000");
+        }
 
         //Annual Interest Rate
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEARS;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest > 0 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEARS;
+                break;
+            }
+            System.out.println("Enter a number greater than 0 and less than or equal to 30");
+        }
 
         //Period
-        System.out.print("Period (Years): ");
-        byte period = scanner.nextByte();
-        int numberOfPayments = period * MONTHS_IN_YEARS;
+        while (true) {
+            System.out.print("Period (Years): ");
+            byte period = scanner.nextByte();
+            if (period >= 1 && period <= 30) {
+                numberOfPayments = period * MONTHS_IN_YEARS;
+                break;
+            }
+            System.out.println("Enter a number between 1 and 30");
+        }
 
         //Mortgage Formula
         double interest = Math.pow( ( 1 + monthlyInterest ), numberOfPayments );
